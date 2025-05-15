448c0bc738be7724c4e52d625ed625109ec7e60f
Stefano Babini <stefano.babini5@studio.unibo.it>
2025-05-15 09:55:39 +0200

    drawable

--- src/main/scala/it/unibo/pcd/akka/basics/e11gui/actors/DrawerActor.scala
+++ src/main/scala/it/unibo/pcd/akka/basics/e11gui/actors/DrawerActor.scala
@@ -1,7 +1,6 @@
 package it.unibo.pcd.akka.basics.e11gui.actors
 
 import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
 import it.unibo.pcd.akka.basics.e11gui.view.{
   DrawablePanel,
   DrawableRectangle,
@@ -16,6 +15,11 @@ import scala.swing.SimpleSwingApplication
 import scala.swing.event.MousePressed
 import scala.swing.{Dimension, Frame, MainFrame, SimpleSwingApplication}
 import scala.util.Success
+import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.{Behaviors, TimerScheduler}
+import it.unibo.pcd.akka.basics.e11gui.actors.TimerTick.Tick
+
+import scala.concurrent.duration.*
 
 enum DrawMessage:
   case Rectangle(x: Int, y: Int, width: Int, height: Int)
@@ -29,6 +33,9 @@ object DrawerActor:
 
 enum ClickActorListener:
   case Click(x: Int, y: Int)
+
+enum TimerTick:
+  case Tick()
 
 object ClickActor:
   def apply[G](panel: DrawablePanel[G], drawer: akka.actor.typed.ActorRef[DrawMessage]): Behavior[ClickActorListener] =
@@ -45,11 +52,32 @@ object ClickActor:
           drawer ! DrawMessage.Rectangle(x, y, 5, 5)
           Behaviors.same
 
+object RandomDrawerActor:
+  def apply[G](drawer: akka.actor.typed.ActorRef[DrawMessage], panel: DrawablePanel[G]): Behavior[TimerTick] =
+    def randomX = (Math.random() * 400).toInt
+    def randomY = (Math.random() * 300).toInt
+    Behaviors.receiveMessage:
+      case TimerTick.Tick() =>
+        drawer ! DrawMessage.Rectangle(randomX, randomY, 5, 5)
+        Behaviors.same
+
+object TimerActor:
+  def apply[G](randomDrawerActor: akka.actor.typed.ActorRef[TimerTick]): Behavior[TimerTick] =
+    Behaviors.withTimers(timers =>
+      timers.startTimerWithFixedDelay(TimerTick.Tick(), 1.second)
+      Behaviors.receiveMessage:
+        case TimerTick.Tick() =>
+          randomDrawerActor ! TimerTick.Tick()
+          Behaviors.same
+    )
+
 object MainActor:
   def apply[G](panel: DrawablePanel[G], factory: ElementFactory[G]): Behavior[Nothing] =
     Behaviors.setup: context =>
       val drawer = context.spawn(DrawerActor[G](panel, factory), "drawer")
       val clickActor = context.spawn(ClickActor(panel, drawer), "clickActor")
+      val randomDrawerActor = context.spawn(RandomDrawerActor(drawer, panel), "randomActor")
+      val timerActor = context.spawn(TimerActor(randomDrawerActor), "timerActor")
       Behaviors.empty
 
 object MyDrawingApp extends SimpleSwingApplication:
